dff048f83fd9739b416082c90e991765ac75857f
sazenice <tomhal.mc@gmail.com>
2026-04-20 22:02:42 +0200

    Oprava v ToggleFDB

--- src/main/java/cz/sk/corrupted/universe/fakeDeathBan/commands/ToggleFDB.java
+++ src/main/java/cz/sk/corrupted/universe/fakeDeathBan/commands/ToggleFDB.java
@@ -13,7 +13,7 @@ public class ToggleFDB implements CommandExecutor {
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, String @NonNull [] args) {
         FakeDeathBan.isEnabled = !FakeDeathBan.isEnabled;
         String toSend;
-        if (FakeDeathBan.isEnabled){toSend = "vypli";}else{toSend = "zapli";}
+        if (FakeDeathBan.isEnabled){toSend = "zapli";}else{toSend = "vypli";}
         sender.sendMessage(FakeDeathBan.prefix + ChatColor.GREEN + Messages.getMessage("toggle", toSend));
         return true;
     }
